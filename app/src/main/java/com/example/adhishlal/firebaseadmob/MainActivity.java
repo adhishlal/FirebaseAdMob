25f4926c412c1299d40f1003c894c96bc73b3a3b
adhishlal <adhish@adhishlal.com>
2016-07-03 09:04:57 +0530

    Initial commit

---
+++ app/src/main/java/com/example/adhishlal/firebaseadmob/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.adhishlal.firebaseadmob;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MobileAds.initialize(getApplicationContext(), "ca-app-pub-2425943660599811~3924545881");
+
+        AdView mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+    }
+}
